e4c8f8fd7c7ccb5bb237383d53e452f603dd4c48
VickFluchaire <victor.fluchaire@iut-valence.fr>
2016-03-08 14:32:09 +0100

    We continue the JavaDoc and try to know what is important for the game.

---
+++ src/HotJavaApp.java
@@ -0,0 +1,22 @@
+
+/**
+ * Represent application of the game HotJava
+ * @author Fluchaire
+ * @author Delaunay
+ */
+
+public class HotJavaApp
+{
+
+	/**
+	 * Call to HotJavaGame class
+	 * @param args ...
+	 */
+	
+	public static void main(String[] args)
+	{
+		new HotJavaGame().play();
+
+	}
+
+}
---
+++ src/HotJavaGame.java
@@ -0,0 +1,43 @@
+
+/**
+ * Represent a game, new game + the concept
+ * @author Fluchaire
+ * @author Delaunay
+ */
+
+public class HotJavaGame
+{
+	/**
+	 * ...
+	 */
+	private int turn;
+		
+	
+	// All tiles need to be reverse and the coffee is not represented
+	
+	/**
+	 * Create a new HotJava game, ready to be played
+	 * We have a square tray with tiles, all tiles are reverse
+	 * The coffee take a randomly tile out of the square but on the left side
+	 * The coffee cup is place randomly on the right side
+	 * Turn is initialize at 0
+	 * 
+	 */
+	
+	public HotJavaGame()
+	{
+		this.turn = 0;
+		
+	}
+		
+	/** 
+	 * Play the game
+	 * The  
+	 */
+	
+	public void play()
+	{
+			
+	}
+
+}
