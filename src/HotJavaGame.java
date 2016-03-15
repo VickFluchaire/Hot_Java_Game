58367b2dda80e8fdd86ee5c1321bd1fb92bfb48b
VickFluchaire <victor.fluchaire@iut-valence.fr>
2016-03-15 09:32:24 +0100

    Task DONE 2/3 Add JavaDoc, board initialized

--- src/HotJavaGame.java
+++ src/HotJavaGame.java
@@ -7,38 +7,79 @@
 public class HotJavaGame
 {
 	/**
-	 * ...
+	 * Attributes
+	 * 
+	 * -nbTurn create to count the total number of turn during a game
 	 */
-	private int turn;
-		
-	
-	// All tiles need to be reverse and the coffee is not represented
-	
+	private int nbTurn;
+	/**
+	 * -countdown is create to show to the player when the coffee will flow
+	 */
+	private int countdown;
+	/**
+	 * -nbline represent the line's number in the board
+	 */
+	private int nbLine;
+	/**
+	 * -nbCol represent the column's number in the board
+	 */
+	private int nbCol;
+	/**
+	 * -board is the main board 
+	 */
+	private char[][] board;
+
+
 	/**
 	 * Create a new HotJava game, ready to be played
-	 * The board is initialized with reversed tiles
+	 * The board is a table (an other object) initialized with reversed tiles
 	 * The coffee comes from a random tile out of the square on the left side
 	 * The coffee cup is placed randomly on the right side
-	 * Turn is initialized at 0
-	 * 
+	 * countdown is initialized at 10
+	 * nbTurn is initialized at 0
 	 */
 	
-	public HotJavaGame()
+	public HotJavaGame(int n, int p)
 	{
-		this.turn = 0;
+		this.nbTurn = 0;
+		this.countdown = 10;
+		this.nbLine = n;
+		this.nbCol = p;
+		this.board = new char [nbLine][nbCol];
 		
-		// TODO how the board is represented, and initialized ?		
+		// TODO (DONE) how the board is represented, and initialized ?
 	}
 		
-	// TODO describe the main algorithm
+	// TODO (DONE) describe the main algorithm
 	/** 
-	 * Plays the game
-	 * The  
+	 * Start the game
+	 * 
+	 * countdown and nbTurn are initialized
+	 * 
+	 * According to countdown 
+	 * 		while countdown > 0
+	 * 			The player can do 1 action;
+	 * 				// 1 action = returning 1 tile OR Swap two tiles
+	 * 			countdown --;
+	 * 			nbTurn ++;
+	 * 		When countdown = 0 the coffee begin fill tile
+	 * 			if the coffee can't advance
+	 * 				the player lose the game;
+	 * 				display ("You lose", nbTurn);
+	 * 				The player have the choice to restart the game
+	 * 			else
+	 * 				the player can continue the game;
+	 * 				reinitialized countdown at 10;
+	 * 
+	 * Except case :
+	 * When the coffee reach the cup
+	 * 		display ("You win",nbTurn)
+	 * 
 	 */
 	
 	public void play()
 	{
-			
+		
 	}
 
 }
